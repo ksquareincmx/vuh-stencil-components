1d24b5eb3a678701bdbb95d30ce3ad637fa30320
Abraham Berzunza <jesus.berzunza@theksquaregroup.com>
2020-06-05 19:06:14 +0000

    Feature/typography

--- core/stencil.config.ts
+++ core/stencil.config.ts
@@ -1,30 +1,31 @@
-import { Config } from "@stencil/core";
-import { sass } from "@stencil/sass";
+import { Config } from '@stencil/core';
+import { sass } from '@stencil/sass';
 
 export const config: Config = {
-  namespace: "k-ui-kit",
-  taskQueue: "async",
+  namespace: 'k-ui-kit',
+  taskQueue: 'async',
   outputTargets: [
     {
-      type: "dist",
-      esmLoaderPath: "../loader",
+      type: 'dist',
+      esmLoaderPath: '../loader'
     },
     {
-      type: "docs-readme",
+      type: 'docs-readme'
     },
     {
-      type: "dist",
-      esmLoaderPath: "../loader",
-      copy: [{ src: "**/*.scss" }],
+      type: 'dist',
+      esmLoaderPath: '../loader',
+      copy: [{ src: '**/*.scss' }]
     },
     {
-      type: "www",
-      serviceWorker: null, // disable service workers
-    },
+      type: 'www',
+      serviceWorker: null // disable service workers
+    }
   ],
   plugins: [
     sass({
-      injectGlobalPaths: ["src/css/variables.scss"],
-    }),
+      injectGlobalPaths: ['src/styles/global.bundle.scss']
+    })
   ],
+  copy: [{ src: 'fonts', dest: 'fonts' }]
 };
