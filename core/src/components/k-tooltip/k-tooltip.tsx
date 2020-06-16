36eb0040231aa3849d7d9673be0056a91ee6d1f9
Abraham Berzunza <jesus.berzunza@theksquaregroup.com>
2020-06-16 17:56:25 +0000

    Feature/tooltip

---
+++ core/src/components/k-tooltip/k-tooltip.tsx
@@ -0,0 +1,20 @@
+import { Component, h, Prop } from '@stencil/core';
+
+@Component({
+  tag: 'k-tooltip',
+  styleUrl: 'k-tooltip.scss',
+  shadow: true
+})
+export class KTooltip {
+  @Prop() text: string = '';
+  @Prop() position: 'top' | 'left' | 'right' | 'bottom' = 'bottom';
+
+  render() {
+    return (
+      <div class="KTooltip">
+        <slot></slot>
+        <span class={`KTooltip-text ${this.position}`}>{this.text}</span>
+      </div>
+    );
+  }
+}
