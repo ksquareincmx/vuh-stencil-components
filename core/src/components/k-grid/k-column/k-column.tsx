5f753253ed69beea4af562626febfa908e3d3837
eduardomonforte <eduardomonforte@gmail.com>
2020-06-18 09:02:33 -0500

    Made column props naming less redundant.

--- core/src/components/k-grid/k-column/k-column.tsx
+++ core/src/components/k-grid/k-column/k-column.tsx
@@ -7,54 +7,46 @@ import { Component, h, Host, Prop } from '@stencil/core';
 })
 export class KColumn {
   @Prop() size?:
-    | 'col-1'
-    | 'col-2'
-    | 'col-3'
-    | 'col-4'
-    | 'col-5'
-    | 'col-6'
-    | 'col-7'
-    | 'col-8'
-    | 'col-9'
-    | 'col-10'
-    | 'col-11'
-    | 'col-12';
+    | '1'
+    | '2'
+    | '3'
+    | '4'
+    | '5'
+    | '6'
+    | '7'
+    | '8'
+    | '9'
+    | '10'
+    | '11'
+    | '12';
 
-  @Prop() sizeSm?: 'col-sm-1' | 'col-sm-2' | 'col-sm-3' | 'col-sm-4';
+  @Prop() sizeSm?: '1' | '2' | '3' | '4';
 
-  @Prop() sizeMd?:
-    | 'col-md-1'
-    | 'col-md-2'
-    | 'col-md-3'
-    | 'col-md-4'
-    | 'col-md-5'
-    | 'col-md-6'
-    | 'col-md-7'
-    | 'col-md-8';
+  @Prop() sizeMd?: '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8';
 
   @Prop() sizeLg?:
-    | 'col-lg-1'
-    | 'col-lg-2'
-    | 'col-lg-3'
-    | 'col-lg-4'
-    | 'col-lg-5'
-    | 'col-lg-6'
-    | 'col-lg-7'
-    | 'col-lg-8'
-    | 'col-lg-9'
-    | 'col-lg-10'
-    | 'col-lg-11'
-    | 'col-lg-12';
+    | '1'
+    | '2'
+    | '3'
+    | '4'
+    | '5'
+    | '6'
+    | '7'
+    | '8'
+    | '9'
+    | '10'
+    | '11'
+    | '12';
 
   @Prop() color?: string;
 
   render() {
     return (
       <Host
-        class={`KColumn ${this.size ? 'KColumn--' + this.size : ''} ${
-          this.sizeSm ? 'KColumn--' + this.sizeSm : ''
-        } ${this.sizeMd ? 'KColumn--' + this.sizeMd : ''} ${
-          this.sizeLg ? 'KColumn--' + this.sizeLg : ''
+        class={`KColumn ${this.size ? 'KColumn--col-' + this.size : ''} ${
+          this.sizeSm ? 'KColumn--col-sm-' + this.sizeSm : ''
+        } ${this.sizeMd ? 'KColumn--col-md-' + this.sizeMd : ''} ${
+          this.sizeLg ? 'KColumn--col-lg-' + this.sizeLg : ''
         }`}
       >
         <p class={`KColumn-content`}>
