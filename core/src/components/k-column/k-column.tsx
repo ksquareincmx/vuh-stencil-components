46db77e55f768ad44e389b5a8e8006650a79b3a6
Eduardo Monforte <eduardo.monforte@theksquaregroup.com>
2020-06-22 15:21:30 +0000

    Row spacing, new column sizes, checkbox style fix.

--- core/src/components/k-column/k-column.tsx
+++ core/src/components/k-column/k-column.tsx
@@ -1,4 +1,5 @@
 import { Component, h, Host, Prop } from '@stencil/core';
+import clsx from 'clsx';
 
 @Component({
   tag: 'k-column',
@@ -6,6 +7,8 @@ import { Component, h, Host, Prop } from '@stencil/core';
   shadow: true
 })
 export class KColumn {
+  @Prop() fluid?: boolean = false;
+
   @Prop() size?:
     | '1'
     | '2'
@@ -20,11 +23,49 @@ export class KColumn {
     | '11'
     | '12';
 
-  @Prop() sizesm?: '1' | '2' | '3' | '4';
+  @Prop() sizesm?:
+    | '1'
+    | '2'
+    | '3'
+    | '4'
+    | '5'
+    | '6'
+    | '7'
+    | '8'
+    | '9'
+    | '10'
+    | '11'
+    | '12';
 
-  @Prop() sizemd?: '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8';
+  @Prop() sizemd?:
+    | '1'
+    | '2'
+    | '3'
+    | '4'
+    | '5'
+    | '6'
+    | '7'
+    | '8'
+    | '9'
+    | '10'
+    | '11'
+    | '12';
 
   @Prop() sizelg?:
+    | '1'
+    | '2'
+    | '3'
+    | '4'
+    | '5'
+    | '6'
+    | '7'
+    | '8'
+    | '9'
+    | '10'
+    | '11'
+    | '12';
+
+  @Prop() sizexl?:
     | '1'
     | '2'
     | '3'
@@ -43,11 +84,15 @@ export class KColumn {
   render() {
     return (
       <Host
-        class={`KColumn ${this.size ? 'KColumn--col-' + this.size : ''} ${
-          this.sizesm ? 'KColumn--col-sm-' + this.sizesm : ''
-        } ${this.sizemd ? 'KColumn--col-md-' + this.sizemd : ''} ${
-          this.sizelg ? 'KColumn--col-lg-' + this.sizelg : ''
-        }`}
+        class={clsx(
+          `KColumn`,
+          [this.fluid && `KColumn--fluid`],
+          [this.size && `KColumn--col-${this.size}`],
+          [this.sizesm && `KColumn--col-sm-${this.sizesm}`],
+          [this.sizemd && `KColumn--col-md-${this.sizemd}`],
+          [this.sizelg && `KColumn--col-lg-${this.sizelg}`],
+          [this.sizexl && `KColumn--col-xl-${this.sizexl}`]
+        )}
       >
         <div class={`KColumn-content`}>
           <slot />
