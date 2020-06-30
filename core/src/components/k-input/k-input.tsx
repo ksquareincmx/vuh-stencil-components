589520e6ac8d263f0075c863f61c8f77edb65dce
abrahamBerzunza <abraham.berzunza@gmail.com>
2020-06-30 15:59:45 -0500

    k-input validation state property added

--- core/src/components/k-input/k-input.tsx
+++ core/src/components/k-input/k-input.tsx
@@ -1,4 +1,5 @@
 import { Component, h, Prop } from '@stencil/core';
+import clsx from 'clsx';
 
 @Component({
   tag: 'k-input',
@@ -8,21 +9,57 @@ import { Component, h, Prop } from '@stencil/core';
 export class KInput {
   @Prop() type: string = 'text';
   @Prop() label: string = 'Default';
-  @Prop() value: string = '';
-  @Prop() disabled: boolean = false;
-  @Prop() validationState: '' | 'success' | 'error' = '';
+  @Prop() value?: string = '';
+  @Prop() disabled?: boolean = false;
+  @Prop() validationState?: '' | 'success' | 'error' = '';
+  @Prop() name?: string = '';
+
+  private isSuccess = () => {
+    return this.validationState === 'success';
+  };
+
+  private isError = () => {
+    return this.validationState === 'error';
+  };
 
   render() {
     return (
       <div class="KInput">
         <input
+          class={clsx({
+            '--is-valid': this.isSuccess(),
+            '--is-invalid': this.isError()
+          })}
           id="k-input"
           placeholder=" "
           value={this.value}
           disabled={this.disabled}
           type={this.type}
+          name={this.name}
         />
-        <label htmlFor="k-input">{this.label}</label>
+        <label
+          htmlFor="k-input"
+          class={clsx({
+            '--is-valid': this.isSuccess(),
+            '--is-invalid': this.isError()
+          })}
+        >
+          {this.label}
+        </label>
+        <div
+          class={clsx('KInput-icon', {
+            '--is-valid': this.isSuccess(),
+            '--is-invalid': this.isError()
+          })}
+        >
+          {this.isSuccess() && (
+            <k-icon size="medium" name="check_circle"></k-icon>
+          )}
+
+          {this.isError() && (
+            <k-icon size="medium" name="alert_circle"></k-icon>
+          )}
+        </div>
       </div>
     );
   }
