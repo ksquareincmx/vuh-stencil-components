dd1389a5ec14ff66aca5ff023325e606b516e2ea
abrahamBerzunza <abraham.berzunza@gmail.com>
2020-08-03 11:19:33 -0500

    modal component added

---
+++ core/src/components/k-modal/k-modal.tsx
@@ -0,0 +1,50 @@
+import { Component, h, Prop, State } from '@stencil/core';
+import clsx from 'clsx';
+
+@Component({
+  tag: 'k-modal',
+  styleUrl: 'k-modal.scss',
+  shadow: true
+})
+export class KModal {
+  @Prop({ mutable: true }) show: boolean = false;
+  @Prop() hideCloseButton: boolean = false;
+  @State() body?: HTMLElement;
+
+  componentWillRender() {
+    this.body = document.querySelector('body');
+
+    if (this.show) {
+      this.body.style.overflow = 'hidden';
+    }
+  }
+
+  handleCloseModal() {
+    this.show = false;
+    this.body.style.overflow = 'auto';
+  }
+
+  render() {
+    return (
+      <div
+        class={clsx('KModal', {
+          '--is-showed': this.show
+        })}
+      >
+        <div class="KModal-content">
+          {!this.hideCloseButton && (
+            <div class="KModal-close">
+              <i
+                class="KModal-btn vuh-close"
+                onClick={this.handleCloseModal.bind(this)}
+              ></i>
+            </div>
+          )}
+          <div class="KModal-body">
+            <slot></slot>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
