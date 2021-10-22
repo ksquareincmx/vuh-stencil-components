e42160b4ce91d09204a6d97b5ace11d53734bc9e
Fernando Gómez <jfernando.gomez@theksquaregroup.com>
2021-10-22 14:51:25 -0500

    Highlight style to code editor added

--- core/src/components/playground/playground.stories.tsx
+++ core/src/components/playground/playground.stories.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import CodeViewer from '../../utils/CodeViewer.jsx';
 import './playground.css';
 import {
@@ -13,34 +13,60 @@ export default {
   title: 'Playground'
 }
 
-const initialCode = `
-<>
-  Write here (keep the empty tags)
-</>
-`;
+const initialCode = 
+`<>
+  <h2>Write here (keep the empty tags)</h2>
+</>`;
 
 const Template = () => {
 
   const [code, setCode] = useState(initialCode);
 
+  const ref = useRef(null);
+
   const handleChange = (e) => {
     setCode(e.target.value);
   }
 
+  useEffect(() => {
+    if (ref && ref.current) {
+      ref.current.addEventListener('keydown', autosize)
+    }
+  })
+
+  function autosize(e){
+    var el = this;
+    setTimeout(function(){
+      el.style.cssText = 'height:auto; padding:0';
+      el.style.cssText = 'height:' + el.scrollHeight + 'px';
+    },0);
+    if ( e.which != 9 ) return;
+    e.preventDefault();
+  }
+
   return (
   <div className="story-container k-playground">
-    <textarea name="" className="writer" value={code} onChange={handleChange}></textarea>
-  
-  <CodeViewer keepCode={true}>
-{code}
-  </CodeViewer>
-  <div id="live-editor">
-    <LiveProvider code={code}>
-      <LiveEditor />
-      <LiveError />
-      <LivePreview />
-    </LiveProvider>
-  </div>
+    <CodeViewer keepCode={true}>
+      {code}
+    </CodeViewer>
+
+    <div id="live-editor">
+      <LiveProvider code={code}>
+        <LiveEditor />
+        <LiveError />
+        <LivePreview />
+      </LiveProvider>
+    </div>
+
+    <textarea
+      className="writer"
+      name=""
+      ref={ref}
+      rows={4}
+      value={code}
+      onChange={handleChange}
+      spellCheck={false}
+    ></textarea>
   </div>
   )
 };
